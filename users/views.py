e747a8f7b557565cdd3edccf9c30f807209097b8
jack <3hellspawn3@gmail.com>
2020-02-16 21:15:09 +0200

    Fixed registration and login breach

--- users/views.py
+++ users/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
+from django.http import Http404
 
 def register(request):
-    """Register a new user."""    
-    if request.method != 'POST':
-        # Display blank registration form.
-        form = UserCreationForm()
+    """Register a new user."""
+    if request.user.is_authenticated:
+        raise Http404
     else:
-        # Process completed form.
-        form = UserCreationForm(data=request.POST)
-        
-        if form.is_valid():
-            new_user = form.save()
-            # Log the user in and then redirect to home page.
-            login(request, new_user)
-            return redirect('learning_logs:index')
+        if request.method != 'POST':
+            # Display blank registration form.
+            form = UserCreationForm()
+        else:
+            # Process completed form.
+            form = UserCreationForm(data=request.POST)
             
-    # Display a blank or invalid form.
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+            if form.is_valid():
+                new_user = form.save()
+                # Log the user in and then redirect to home page.
+                login(request, new_user)
+                return redirect('learning_logs:index')
+                
+        # Display a blank or invalid form.
+        context = {'form': form}
+        return render(request, 'registration/register.html', context)
+    
